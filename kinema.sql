6f8ffb9921de35057c9d3647f21c9c1df898f4f4
alessia-cidri-rpg <alessia.cidri@fshnstudent.info>
2026-04-28 13:13:28 +0200

    bogos binted

--- kinema.sql
+++ kinema.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 19, 2026 at 06:38 PM
+-- Generation Time: Apr 19, 2026 at 09:28 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,13 +24,24 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `aktore`
+--
+
+CREATE TABLE `aktore` (
+  `aktor_id` int(11) NOT NULL,
+  `aktor_emri` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `filmi`
 --
 
 CREATE TABLE `filmi` (
   `filmi_id` int(11) NOT NULL,
   `regjisor_id` int(11) DEFAULT NULL,
-  `titulli_id` varchar(200) DEFAULT NULL,
+  `titulli` varchar(100) DEFAULT NULL,
   `status` varchar(20) DEFAULT NULL,
   `kohezgjatja` int(11) DEFAULT NULL,
   `data` date DEFAULT NULL,
@@ -52,15 +63,57 @@ CREATE TABLE `regjisor` (
   `regjisor_emri` varchar(500) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `zhanri`
+--
+
+CREATE TABLE `zhanri` (
+  `zhanri_id` int(11) NOT NULL,
+  `zhanri_emri` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `zhanri`
+--
+
+INSERT INTO `zhanri` (`zhanri_id`, `zhanri_emri`) VALUES
+(1, 'Aksion'),
+(2, 'Aventure'),
+(3, 'Animacion'),
+(4, 'Komedi'),
+(5, 'Fantazi'),
+(6, 'Drame'),
+(7, 'Thriller');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `zhanri_filmi`
+--
+
+CREATE TABLE `zhanri_filmi` (
+  `filmi_id` int(11) DEFAULT NULL,
+  `zhanri_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `aktore`
+--
+ALTER TABLE `aktore`
+  ADD PRIMARY KEY (`aktor_id`);
+
+--
 -- Indexes for table `filmi`
 --
 ALTER TABLE `filmi`
-  ADD PRIMARY KEY (`filmi_id`);
+  ADD PRIMARY KEY (`filmi_id`),
+  ADD KEY `fk_filmi_regjisor` (`regjisor_id`);
 
 --
 -- Indexes for table `regjisor`
@@ -69,8 +122,27 @@ ALTER TABLE `regjisor`
   ADD PRIMARY KEY (`regjisor_id`);
 
 --
+-- Indexes for table `zhanri`
+--
+ALTER TABLE `zhanri`
+  ADD PRIMARY KEY (`zhanri_id`);
+
+--
+-- Indexes for table `zhanri_filmi`
+--
+ALTER TABLE `zhanri_filmi`
+  ADD KEY `fk_zhanrifilmi_filmi` (`filmi_id`),
+  ADD KEY `fk_zhanrifilmi_zhanri` (`zhanri_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `aktore`
+--
+ALTER TABLE `aktore`
+  MODIFY `aktor_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `filmi`
@@ -83,6 +155,23 @@ ALTER TABLE `filmi`
 --
 ALTER TABLE `regjisor`
   MODIFY `regjisor_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `filmi`
+--
+ALTER TABLE `filmi`
+  ADD CONSTRAINT `fk_filmi_regjisor` FOREIGN KEY (`regjisor_id`) REFERENCES `regjisor` (`regjisor_id`);
+
+--
+-- Constraints for table `zhanri_filmi`
+--
+ALTER TABLE `zhanri_filmi`
+  ADD CONSTRAINT `fk_zhanrifilmi_filmi` FOREIGN KEY (`filmi_id`) REFERENCES `filmi` (`filmi_id`),
+  ADD CONSTRAINT `fk_zhanrifilmi_zhanri` FOREIGN KEY (`zhanri_id`) REFERENCES `zhanri` (`zhanri_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
